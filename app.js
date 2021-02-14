56b85bf460b71f066c15d23a81f7ae94446111f1
dragon9481 <77512206+dragon9481@users.noreply.github.com>
2021-02-14 13:24:28 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// Variables
-let contador = 0
-const cero = true
-
-// Funciones
-function countingClicks() {
-    contador = contador+1
-    document.getElementById("counting").innerHTML = contador;
-}
